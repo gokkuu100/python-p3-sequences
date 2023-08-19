debafe0bd2b1aac0a3b89a11976939785f86eb3f
goku <wkurts247@gmail.com>
2023-08-19 17:55:02 +0300

    Completed assignemnt

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0,1]
+
+    while len(fibonacci) < length:
+        nextNumber = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(nextNumber)
+    
+    for x in fibonacci:
+        print(x)
+
+print(print_fibonacci(9))
